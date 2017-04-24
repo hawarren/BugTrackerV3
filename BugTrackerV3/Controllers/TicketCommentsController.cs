80778dfa09010811851bf29575762f9bedb32e98
Hanif Warren-desktop <hanif.warren@gmail.com>
2017-04-24 05:29:59 -0400

    set up rudimentary ticket comments. still need to refine, and also do ticket attachments

---
+++ BugTrackerV3/Controllers/TicketCommentsController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using BugTrackerV3.Models;
+
+namespace BugTrackerV3.Controllers
+{
+    public class TicketCommentsController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: TicketComments
+        public ActionResult Index()
+        {
+            var ticketComments = db.TicketComments.Include(t => t.Ticket).Include(t => t.User);
+            return View(ticketComments.ToList());
+        }
+
+        // GET: TicketComments/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TicketComment ticketComment = db.TicketComments.Find(id);
+            if (ticketComment == null)
+            {
+                return HttpNotFound();
+            }
+            return View(ticketComment);
+        }
+
+        // GET: TicketComments/Create
+        public ActionResult Create()
+        {
+            ViewBag.TicketId = new SelectList(db.Tickets, "Id", "Title");
+            ViewBag.UserId = new SelectList(db.Users, "Id", "FirstName");
+            return View();
+        }
+
+        // POST: TicketComments/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Comment,Created,TicketId,UserId")] TicketComment ticketComment)
+        {
+            if (ModelState.IsValid)
+            {
+                db.TicketComments.Add(ticketComment);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.TicketId = new SelectList(db.Tickets, "Id", "Title", ticketComment.TicketId);
+            ViewBag.UserId = new SelectList(db.Users, "Id", "FirstName", ticketComment.UserId);
+            return View(ticketComment);
+        }
+
+        // GET: TicketComments/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TicketComment ticketComment = db.TicketComments.Find(id);
+            if (ticketComment == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.TicketId = new SelectList(db.Tickets, "Id", "Title", ticketComment.TicketId);
+            ViewBag.UserId = new SelectList(db.Users, "Id", "FirstName", ticketComment.UserId);
+            return View(ticketComment);
+        }
+
+        // POST: TicketComments/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Comment,Created,TicketId,UserId")] TicketComment ticketComment)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(ticketComment).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.TicketId = new SelectList(db.Tickets, "Id", "Title", ticketComment.TicketId);
+            ViewBag.UserId = new SelectList(db.Users, "Id", "FirstName", ticketComment.UserId);
+            return View(ticketComment);
+        }
+
+        // GET: TicketComments/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TicketComment ticketComment = db.TicketComments.Find(id);
+            if (ticketComment == null)
+            {
+                return HttpNotFound();
+            }
+            return View(ticketComment);
+        }
+
+        // POST: TicketComments/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            TicketComment ticketComment = db.TicketComments.Find(id);
+            db.TicketComments.Remove(ticketComment);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
