81f4e61f010bb88a3bf6bd72c6f3e7e9af88c3ed
Hanif Warren <hanif.warren@gmail.com>
2017-08-02 15:38:12 -0400

    Added Homer files (in alphabetical order) up to and including the Controller folder.

---
+++ BugTrackerV3/App_Start/WebApiConfig.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http; 
+
+using System.Web.UI.WebControls;
+
+namespace BugTrackerV3
+{
+    public static class BugTrackerV3
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
