89307bbfb1b972cc304d12157cabc3bbbda3891a
Hanif Warren <hanif.warren@gmail.com>
2017-08-07 18:00:40 -0400

    Fixed account registration redirect bug by uncommenting line in the line in AccountController

--- BugTrackerV3/Controllers/AccountController.cs
+++ BugTrackerV3/Controllers/AccountController.cs
@@ -183,7 +183,7 @@ namespace BugTrackerV4.Controllers
 
                     helpers.UserRolesHelper helper = new helpers.UserRolesHelper();
                     helper.AddUserToRole(user.Id, "Submitter");
-                    //return RedirectToAction("Index", "Home");
+                    return RedirectToAction("Index", "Home");
                 }
                 AddErrors(result);
             }
