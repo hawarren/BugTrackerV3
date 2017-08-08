83ac9f6b22caf2199bc5675bc58770e85a27d8f0
Hanif Warren <hanif.warren@gmail.com>
2017-08-08 02:49:43 -0400

    fixed global web.config, app builds and runs, need to include all  the Homer views in project.

--- BugTrackerV3/Global.asax.cs
+++ BugTrackerV3/Global.asax.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using BugTrackerV3.App_Start;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
@@ -13,6 +15,8 @@ namespace BugTrackerV3
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
+
+            WebApiConfig.Register(GlobalConfiguration.Configuration);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
