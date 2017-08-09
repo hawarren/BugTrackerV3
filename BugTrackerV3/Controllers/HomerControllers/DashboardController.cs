064605f7d15e1c2d85d78745e528b657ea991eeb
Hanif Warren <hanif.warren@gmail.com>
2017-08-09 08:54:26 -0400

    Changed all homer controller namespaces to BugTrackerV3

--- BugTrackerV3/Controllers/HomerControllers/DashboardController.cs
+++ BugTrackerV3/Controllers/HomerControllers/DashboardController.cs
@@ -5,7 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Mvc.Html;
 
-namespace Homer_MVC.Controllers
+namespace BugTrackerV3.Controllers
 {
     public class DashboardController : Controller
     {
