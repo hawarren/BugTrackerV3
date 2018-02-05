a6d2301c1b335317449a2500a3c20e58dfaa8309
Hanif Warren <hanif.warren@gmail.com>
2018-02-04 20:48:00 -0500

    removed ability for ticket owner to edit ticket. That should be left to admin, pm for that project, or dev assigned to the ticket.

--- BugTrackerV3/Controllers/AccountController.cs
+++ BugTrackerV3/Controllers/AccountController.cs
@@ -195,8 +195,6 @@ namespace BugTrackerV3.Controllers
                     // Send an email with this link
                     string code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
                     var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
-                  //  await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
-                   // await UserManager.SendEmailAsync(user.Id, "Click this <a href=\"" + callbackUrl + "\"> to confirm</a>", "Please click this <a href=\"" + callbackUrl + "\">link right here</a>");
 
                     await UserManager.SendEmailAsync(user.Id, "Confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>",
             "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
