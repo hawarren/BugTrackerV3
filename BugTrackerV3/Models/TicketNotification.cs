19b3e8a75dd1b60722a18e3586601a0ef6f9be6d
Hanif Warren <hanif.warren@gmail.com>
2018-02-22 12:09:53 -0500

    added recipient and sender fields to Ticket Notifications model.

--- BugTrackerV3/Models/TicketNotification.cs
+++ BugTrackerV3/Models/TicketNotification.cs
@@ -8,13 +8,15 @@ namespace BugTrackerV3.Models
     public class TicketNotification
     {
     public int Id { get; set; }
-    public int TicketId { get; set; }
-    public string UserId { get; set; }
-    public string Change { get; set; }
-    public string Details { get; set; }
+    public string Body { get; set; }
     public DateTimeOffset DateNotified { get; set; }
+    public int TicketId { get; set; }
+    //public string UserId { get; set; }
+    public string SenderId { get; set; }
+    public string RecipientId { get; set; }
 
     public virtual Ticket Ticket { get; set; }
     public virtual ApplicationUser User { get; set; }
+    public virtual  ApplicationUser Sender { get; set; }
     }
 }
