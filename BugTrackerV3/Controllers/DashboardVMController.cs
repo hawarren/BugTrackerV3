76d8022af6751079b3f3a98d4df2616987f95a0e
Hanif Warren <hanif.warren@gmail.com>
2018-03-12 15:57:57 -0400

    moved viewmodels to their own folder. Implemented dashboard viewmodel, controller, and beginning of dashboardview

---
+++ BugTrackerV3/Controllers/DashboardVMController.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BugTrackerV3.helpers;
+using BugTrackerV3.Models;
+using BugTrackerV3.ViewModels;
+using Microsoft.AspNet.Identity;
+
+
+namespace BugTrackerV3.Controllers
+{
+    public class DashboardVMController : Controller
+    {
+
+        private ApplicationDbContext db = new ApplicationDbContext();
+        private ProjectsHelper projHelper = new ProjectsHelper();
+        private TicketsHelper tixHelper = new TicketsHelper();
+        private UserRolesHelper roleHelper = new UserRolesHelper();
+
+        public ActionResult BasicDashboard()
+        {
+            var dashboardData = new DashboardVM();
+            var userId = User.Identity.GetUserId();
+            var myRole = "Guest";
+            if (userId != null)
+                myRole = this.roleHelper.ListUserRoles(User.Identity.GetUserId()).FirstOrDefault();
+
+            switch (myRole)
+            {
+                case "Admin":
+                    dashboardData.Projects = this.db.Projects.ToList();
+                    dashboardData.Tickets = this.db.Tickets.ToList();
+                    break;
+
+                case "ProjectManager":
+                    dashboardData.Projects = this.projHelper.ListUserProjects(userId).ToList();
+                    dashboardData.Tickets = this.tixHelper.GetPMTickets();
+                    break;
+
+                case "Developer":
+                    dashboardData.Projects = this.projHelper.ListUserProjects(userId).ToList();
+                    dashboardData.Tickets = this.db.Tickets.Where(t => t.AssignedToUserId == userId).ToList();
+                    break;
+                case "Submitter": dashboardData.Tickets = this.db.Tickets.Where(t => t.OwnerUserId == userId).ToList();
+                    break;
+                default: ViewBag.Message = "You will not be able to see any data until you are assigned a role.";
+                    break;
+            }
+            return View(dashboardData);
+        }
+
+
+        #region DefaultControllerActions
+
+
+
+        //    // GET: DashboardVM
+        //    public ActionResult Index()
+        //    {
+        //        return View();
+        //    }
+
+        //    // GET: DashboardVM/Details/5
+        //    public ActionResult Details(int id)
+        //    {
+        //        return View();
+        //    }
+
+        //    // GET: DashboardVM/Create
+        //    public ActionResult Create()
+        //    {
+        //        return View();
+        //    }
+
+        //    // POST: DashboardVM/Create
+        //    [HttpPost]
+        //    public ActionResult Create(FormCollection collection)
+        //    {
+        //        try
+        //        {
+        //            // TODO: Add insert logic here
+
+        //            return RedirectToAction("Index");
+        //        }
+        //        catch
+        //        {
+        //            return View();
+        //        }
+        //    }
+
+        //    // GET: DashboardVM/Edit/5
+        //    public ActionResult Edit(int id)
+        //    {
+        //        return View();
+        //    }
+
+        //    // POST: DashboardVM/Edit/5
+        //    [HttpPost]
+        //    public ActionResult Edit(int id, FormCollection collection)
+        //    {
+        //        try
+        //        {
+        //            // TODO: Add update logic here
+
+        //            return RedirectToAction("Index");
+        //        }
+        //        catch
+        //        {
+        //            return View();
+        //        }
+        //    }
+
+        //    // GET: DashboardVM/Delete/5
+        //    public ActionResult Delete(int id)
+        //    {
+        //        return View();
+        //    }
+
+        //    // POST: DashboardVM/Delete/5
+        //    [HttpPost]
+        //    public ActionResult Delete(int id, FormCollection collection)
+        //    {
+        //        try
+        //        {
+        //            // TODO: Add delete logic here
+
+        //            return RedirectToAction("Index");
+        //        }
+        //        catch
+        //        {
+        //            return View();
+        //        }
+        //
+    // }
+        #endregion
+    }
+}
---
+++ BugTrackerV3/ViewModels/DashboardVM.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BugTrackerV3.ViewModels
+{
+    using System.Security.Policy;
+
+    using BugTrackerV3.Controllers;
+    using BugTrackerV3.Models;
+
+    public class DashboardVM
+    {
+        public List<Project> Projects { get; set; }
+        public List<Ticket> Tickets { get; set; }
+
+        public DashboardVM()
+        {
+
+            Projects = new List<Project>();
+            Tickets = new List<Ticket>();
+        }
+    }
+}
